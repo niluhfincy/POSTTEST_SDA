db7135ddc4346e52952ad61a2cb85f3390b7f6d6
niluhfincy <niluhfincyglorianathasia@gmail.com>
2025-10-03 21:24:31 +0800

    Add files via upload

---
+++ POSTTEST_4/soal2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node {
+    char data;
+    Node* next;
+};
+
+void push(Node*& top, char data) {
+    Node* newNode = new Node{data, top};
+    top = newNode;
+}
+
+char pop(Node*& top) {
+    if (top == nullptr) return '\0';
+    Node* temp = top;
+    char poppedValue = temp->data;
+    top = top->next;
+    delete temp;
+    return poppedValue;
+}
+
+bool areBracketsBalanced(string expr) {
+    Node* stackTop = nullptr;
+    
+    for (int i = 0; i < expr.length(); i++) {
+        char ch = expr[i];
+        
+        if (ch == '(' || ch == '{' || ch == '[') {
+            push(stackTop, ch);
+        }
+        else if (ch == ')' || ch == '}' || ch == ']') {
+            if (stackTop == nullptr) {
+                return false;
+            }
+            
+            char topChar = pop(stackTop);
+            if ((ch == ')' && topChar != '(') ||
+                (ch == '}' && topChar != '{') ||
+                (ch == ']' && topChar != '[')) {
+                return false;
+            }
+        }
+    }
+    
+    return (stackTop == nullptr);
+}
+
+int main() {
+    string expr1 = "{[()]}";
+    cout << expr1 << " -> " << (areBracketsBalanced(expr1) ? "Seimbang" : "Tidak Seimbang") << endl;
+    
+    string expr2 = "{[(])}";
+    cout << expr2 << " -> " << (areBracketsBalanced(expr2) ? "Seimbang" : "Tidak Seimbang") << endl;
+    
+    return 0;
+}
